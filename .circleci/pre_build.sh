c9058d345a63d795a4de8d99d7822890e7d5cace
mialenguyen <mia.lee1256@gmail.com>
2024-10-08 15:49:26 +0200

    test build without orb

--- .circleci/pre_build.sh
+++ .circleci/pre_build.sh
@@ -7,3 +7,11 @@ function get_env_vars() {
     echo 'export env="stg"' >> "$BASH_ENV"
   fi
 }
+
+function set_up_pre_build() {
+  curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+  cd ~
+  wget https://github.com/digitalocean/doctl/releases/download/v1.110.0/doctl-1.110.0-linux-amd64.tar.gz
+  tar xf ~/doctl-1.110.0-linux-amd64.tar.gz
+  sudo mv ~/doctl /usr/local/bin
+}
